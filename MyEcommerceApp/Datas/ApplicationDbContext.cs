dd1b9c97854ea55b33b271a3fa8675cfeb6119ec
sheng0321 <sxin3160@conestogac.on.ca>
2025-03-20 20:44:59 -0400

    connect to SQLite

---
+++ MyEcommerceApp/Datas/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyEcommerceApp.Datas
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
